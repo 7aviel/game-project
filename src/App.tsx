13520d9b5cf3d0e5047121fdac83d90b62aa5dd8
Ariel <vilcheariel2@gmail.com>
2023-09-11 13:48:44 -0300

    inicio del projecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-/* import ListGroup from "./components/ListGroup"; */
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  interface User {
-    id: number;
-    name: string;
-  }
-
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
-  }, []);
   return (
     <>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <Button colorScheme="blue">Button</Button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.tsx";
-import {} from "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
